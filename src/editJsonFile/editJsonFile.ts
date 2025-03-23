915bb89e8f297532243aaf18710d41f4d39a1671
SURFACEPRO9\mitsu <mitsuhiro.seino@gmail.com>
2025-03-23 19:11:53 +0900

    initial

---
+++ src/editJsonFile/editJsonFile.ts
@@ -0,0 +1,31 @@
+import fs from 'fs-extra';
+import isFunction from 'lodash-es/isFunction';
+import merge from 'lodash-es/merge';
+import path from 'path';
+import checkExistence from '../checkExistence';
+
+/**
+ * JSONファイルを更新する
+ * @param jsonFilePath
+ * @param editor
+ */
+function editJsonFile(
+  jsonFilePath: string,
+  editor: ((obj: any) => object) | object,
+) {
+  const resolvedJsonFilePath = path.resolve(jsonFilePath);
+  if (checkExistence(resolvedJsonFilePath)) {
+    const obj = fs.readJSONSync(resolvedJsonFilePath);
+    let editedJson;
+    if (isFunction(editor)) {
+      // 関数で編集する場合
+      editedJson = editor(obj);
+    } else {
+      // 差分を反映する場合
+      editedJson = merge(obj, editor);
+    }
+    fs.writeJSONSync(resolvedJsonFilePath, editedJson, { spaces: 2 });
+  }
+}
+
+export default editJsonFile;
